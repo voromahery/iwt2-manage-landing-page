bf646aae3e89469a084cf857b5c6d0a8d3540aa9
voromahery <daniel.her@onja.org>
2021-02-02 15:30:19 +0300

    Fixed the appearance of the exta section

--- webroot/index.js
+++ webroot/index.js
@@ -1,26 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+if (window.matchMedia("(max-width: 1000px)").matches) {
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("testimony__card");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
   }
 
-  slides[slideIndex - 1].style.display = "block";
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("testimony__card");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+  }
 }
